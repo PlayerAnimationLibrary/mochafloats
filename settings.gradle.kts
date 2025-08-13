1de8d31fd6d18c4328766a994e2f16348755ab0e
dima_dencep <dima_dencep@redlance.org>
2025-08-14 01:17:07 +0700

    Update buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
-rootProject.name = "mocha"
+pluginManagement {
+    repositories {
+        maven("https://maven.neoforged.net/releases")
+        gradlePluginPortal()
+    }
+}
 
+rootProject.name = "mocha"
